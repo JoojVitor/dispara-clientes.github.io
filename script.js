2b15ea9657a51a4c92c35394b38484668bfccaec
JoojVitor <joaovitordossantoscouto@gmail.com>
2022-08-25 16:37:58 -0300

    tira essa merda

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ function efetueDisparo() {
 
         header = {
             "Authorization": `${$("#auth").val()}`,
-            "Access-Control-Allow-Origin": "*"
         };
 
         $.ajax({
